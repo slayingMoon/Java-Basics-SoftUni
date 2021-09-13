3849358aaeb984be7d1fc5d373a5932b530b702e
slayingMoon <g.kolev15@gmail.com>
2021-09-13 21:32:35 +0200

    Upload work

---
+++ FirstStepsInCoding_Lab/Greeting.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.printf("Hello, %s!", name);
+    }
+}
