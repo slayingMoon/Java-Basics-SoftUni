25196d9c85c7881f4b6b9ef9a90c777c6862d77a
slayingMoon <g.kolev15@gmail.com>
2021-09-15 20:35:04 +0200

    Upload work

---
+++ FirstStepsInCoding_Exercise/Revision.java
@@ -0,0 +1,10 @@
+public class Revision {
+    public static void main(String[] args) {
+// тип, име, стойност
+        int number = 5;
+        double desimalNumber = 5.6;
+        String name = "Pesho";
+        char symbol = 'd';
+
+    }
+}
