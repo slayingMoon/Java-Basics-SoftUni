f869802c6fa6c42bc266c6a1365b03dbd4b0fc1e
slayingMoon <g.kolev15@gmail.com>
2021-09-15 20:34:33 +0200

    Upload work

---
+++ FirstStepsInCoding_Exercise/DepositCalculator_01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DepositCalculator_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //1.да прочетем входните данни и да ги съхраним в променливи
+        //2. да изчислим лихвата -> да я съхраним в променлива
+        //3. да изчислим лихвата за месец -> да я съхраним в променлива
+        //4. да изчислим общата сума -> да я съхраним в променлива
+
+        
+    }
+}
