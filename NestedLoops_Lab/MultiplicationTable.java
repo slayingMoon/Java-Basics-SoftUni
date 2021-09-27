c7379ce5b3669d730c0813326814f0993e801e6c
slayingMoon <g.kolev15@gmail.com>
2021-09-27 22:59:31 +0200

    Upload work

---
+++ NestedLoops_Lab/MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int num1 = 1; num1 <=10 ; num1++) {
+            for (int num2 = 1; num2 <= 10 ; num2++) {
+                System.out.println(num1 + " * " + num2 + " = " + num1 * num2);
+            }
+        }
+    }
+}
