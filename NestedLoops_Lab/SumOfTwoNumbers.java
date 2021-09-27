441fe36b71bad8ec7645fd1697a4857c14be48cb
slayingMoon <g.kolev15@gmail.com>
2021-09-27 22:59:46 +0200

    Upload work

---
+++ NestedLoops_Lab/SumOfTwoNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+        int magicNumber = Integer.parseInt(scanner.nextLine());
+        int count = 0;
+        boolean isFound = false;
+
+        for (int firstNumber = start; firstNumber <= end ; firstNumber++) {
+            for (int secondNumber = start; secondNumber <= end ; secondNumber++) {
+                count++;
+                if (firstNumber + secondNumber == magicNumber) {
+                    isFound = true;
+                    System.out.printf("Combination N:%d (%d + %d = %d)", count,firstNumber,secondNumber,magicNumber);
+                    break;
+                }
+            }
+            if (isFound) {
+                break;
+            }
+        }
+        if (isFound == false) {
+            System.out.printf("%d combinations - neither equals %d", count, magicNumber);
+        }
+    }
+}
